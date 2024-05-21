18fe1d9d5aa11537d59100a4cb86f17422f74bed
shizhong <shizhong1989@gmail.com>
2024-05-21 10:04:42 -0700

    Add create EmployeeRepository

--- ems-backend/src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
+++ ems-backend/src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package net.javaguides.ems.repository;
 
-public interface EmployeeRepository {
+import net.javaguides.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
