d0de0f128f0195ea604965973a008df1c052c017
shizhong <shizhong1989@gmail.com>
2024-05-21 11:12:45 -0700

    Build Get Employees REST API

--- ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
--- ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.ems.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.exception.ResourceNotFoundException;
 import net.javaguides.ems.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import net.javaguides.ems.service.EmployeeService;
@@ -23,6 +24,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exists with given id : " + employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
+
     }
 
 }
