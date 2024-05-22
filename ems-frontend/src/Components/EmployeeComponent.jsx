8a6c3669c600f31baaf2740bd3098d85a55ea625
shizhong <shizhong1989@gmail.com>
2024-05-22 16:58:03 -0700

    Connect Employee Form Validation

--- ems-frontend/src/Components/EmployeeComponent.jsx
+++ ems-frontend/src/Components/EmployeeComponent.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { createEmployee } from "../Services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 function EmployeeComponent() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
 
+  const navigator = useNavigate();
   //   function handleFirstName(e) {
   //     setFirstName(e.target.value);
   //   }
@@ -21,6 +24,10 @@ function EmployeeComponent() {
     e.preventDefault();
     const employee = { firstName, lastName, email };
     console.log(employee);
+    createEmployee(employee).then((response) => {
+      console.log(response);
+      navigator("/employees");
+    });
   }
 
   return (
