727868b14a14b1026e97f8940b6affc9e6371f8b
shizhong <shizhong1989@gmail.com>
2024-05-22 11:33:13 -0700

    Configure Routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import FooterComponent from "./Components/FooterComponent";
 import HeaderComponent from "./Components/HeaderComponent";
 import ListEmployeeComponent from "./Components/ListEmployeeComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* // http://localhost:3000 */}
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/EmployeeService */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
