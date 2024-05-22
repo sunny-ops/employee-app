a82e678475bf2d33d0b61acae37e7d47aebb6b6a
shizhong <shizhong1989@gmail.com>
2024-05-22 11:12:00 -0700

    Connect REact App with REST API

--- ems-frontend/src/Components/ListEmployeeComponent.jsx
+++ ems-frontend/src/Components/ListEmployeeComponent.jsx
@@ -1,26 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { listEmployees } from "../Services/EmployeeService";
 
 const ListEmployeeComponent = () => {
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "Shi",
-      lastName: "Zhong",
-      email: "shizhong@gmail.com",
-    },
-    {
-      id: 2,
-      firstName: "Allen",
-      lastName: "Zhang",
-      email: "allenzhang@gmail.com",
-    },
-    {
-      id: 3,
-      firstName: "Alice",
-      lastName: "Wang",
-      email: "alicewang@gmail.com",
-    },
-  ];
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="container">
       <h2 className="text-center">List of Employees</h2>
@@ -34,7 +25,7 @@ const ListEmployeeComponent = () => {
           </tr>
         </thead>
         <tbody>
-          {dummyData.map((employee) => (
+          {employees.map((employee) => (
             <tr key={employee.id}>
               <td>{employee.id}</td>
               <td>{employee.firstName}</td>
